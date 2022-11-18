9ef042845b0d198d06da59851fe63d3dbfb65b2f
silva <leobatera11091978@gmail.com>
2022-11-17 23:53:42 -0300

    Atualização Final

--- app/src/main/java/com/example/frasesmotivacionais/ui/MainActivity.kt
+++ app/src/main/java/com/example/frasesmotivacionais/ui/MainActivity.kt
@@ -7,13 +7,14 @@ import android.view.WindowId
 import androidx.core.content.ContextCompat
 import com.example.frasesmotivacionais.infra.FrasesmotivacionaisConstants
 import com.example.frasesmotivacionais.R
+import com.example.frasesmotivacionais.data.Mock
 import com.example.frasesmotivacionais.infra.SecurityPreferences
 import com.example.frasesmotivacionais.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
-    private var categoryId = 1
+    private var categoryId = FrasesmotivacionaisConstants.FILTER.ALL
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +27,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         handleUserName()
         handleFilter(R.id.image_all)
+        handleNextPhrase()
 
         // Eventos
         binding.buttonNewPhrase.setOnClickListener(this)
@@ -36,37 +38,41 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(view: View) {
         if (view.id == R.id.button_new_phrase) {
-            var s = ""
+            handleNextPhrase()
         } else if (view.id in listOf(R.id.image_all, R.id.image_happy, R.id.image_sunny)) {
             handleFilter(view.id)
         }
     }
 
-    private fun handleFilter(id: Int) {
+    private fun handleNextPhrase() {
+        binding.textPhrase.text = Mock().getPhrase(categoryId)
+    }
+        private fun handleFilter(id: Int) {
 
-        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
-        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
-        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+            binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+            binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+            binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
 
-        when (id) {
-            R.id.image_all -> {
-                binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
-                categoryId = FrasesmotivacionaisConstants.FILTER.ALL
-            }
-            R.id.image_happy -> {
-                binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
-                categoryId = FrasesmotivacionaisConstants.FILTER.HAPPY
-            }
-            R.id.image_sunny -> {
-                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
-                categoryId = FrasesmotivacionaisConstants.FILTER.SUNNY
+            when (id) {
+                R.id.image_all -> {
+                    binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                    categoryId = FrasesmotivacionaisConstants.FILTER.ALL
+                }
+                R.id.image_happy -> {
+                    binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                    categoryId = FrasesmotivacionaisConstants.FILTER.HAPPY
+                }
+                R.id.image_sunny -> {
+                    binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                    categoryId = FrasesmotivacionaisConstants.FILTER.SUNNY
+                }
             }
         }
-    }
 
-    private fun handleUserName() {
-        val name = SecurityPreferences(this).getString(FrasesmotivacionaisConstants.KEY.USER_NAME)
-        binding.textUserName.text = "Olá, $name!"
+        private fun handleUserName() {
+            val name =
+                SecurityPreferences(this).getString(FrasesmotivacionaisConstants.KEY.USER_NAME)
+            binding.textUserName.text = "Olá, $name!"
 
+        }
     }
-}
